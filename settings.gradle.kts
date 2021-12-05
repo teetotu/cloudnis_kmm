8e827b5143fb6c4752fd334f24279af44f88b074
Shair Dilavar <59974771+teetotu@users.noreply.github.com>
2021-12-05 17:27:50 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMM_Application"
+include(":androidApp")
+include(":shared")
